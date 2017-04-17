258e6ecc6c5d3d32ac6dc277a90502cc7d66ccba
Jacob Kayen <jacob.kayen@gmail.com>
2017-04-17 17:15:22 -0400

    feat: create user model

--- db/models/user.js
+++ db/models/user.js
@@ -2,10 +2,11 @@
 
 // bcrypt docs: https://www.npmjs.com/package/bcrypt
 const bcrypt = require('bcryptjs')
-    , {STRING, VIRTUAL} = require('sequelize')
+    , {STRING, VIRTUAL, BOOLEAN, INTEGER, DATE} = require('sequelize')
 
 module.exports = db => db.define('users', {
-  name: STRING,
+  firstName: STRING,
+  lastName: STRING,
   email: {
     type: STRING,
     validate: {
@@ -13,7 +14,33 @@ module.exports = db => db.define('users', {
       notEmpty: true,
     }
   },
-
+  photo: {
+    type: STRING,
+    validate: {
+      // look out for problems with the isUrl
+      isUrl: true
+    }
+  },
+  admin: {
+    type: BOOLEAN,
+    allowNull: false,
+    defaultValue: true
+  },
+  creditCard: {
+    type: STRING,
+    validate: {
+      isCreditCard: true
+    }
+  },
+  houseNumber: INTEGER,
+  addressLine1: STRING,
+  addressLine2: STRING,
+  city: STRING,
+  // should state be forced as two letters, or left open to the user?
+  state: STRING,
+  dateCreated: {
+    type: DATE,
+  },
   // We support oauth, so users may or may not have passwords.
   password_digest: STRING, // This column stores the hashed password in the DB, via the beforeCreate/beforeUpdate hooks
   password: VIRTUAL // Note that this is a virtual, and not actually stored in DB
@@ -22,6 +49,7 @@ module.exports = db => db.define('users', {
   hooks: {
     beforeCreate: setEmailAndPassword,
     beforeUpdate: setEmailAndPassword,
+    afterCreate: setDate,
   },
   instanceMethods: {
     // This method is a Promisified bcrypt.compare
@@ -40,7 +68,7 @@ module.exports.associations = (User, {OAuth, Thing, Favorite}) => {
   User.belongsToMany(Thing, {as: 'favorites', through: Favorite})
 }
 
-function setEmailAndPassword(user) {
+const setEmailAndPassword = user => {
   user.email = user.email && user.email.toLowerCase()
   if (!user.password) return Promise.resolve(user)
 
@@ -52,3 +80,5 @@ function setEmailAndPassword(user) {
     })
   )
 }
+
+const setDate = () => Date.now();
