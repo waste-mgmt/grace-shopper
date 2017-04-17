739410fc97b6e4cb007fd87805fa4a0a21074378
ekotwick <edmond.kotwick@gmail.com>
2017-04-17 17:09:29 -0400

    create product model for database

---
+++ db/models/product.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const {STRING, TEXT, FLOAT, INTEGER, ENUM} = require('sequelize')
+
+module.exports = db => db.define('product', {
+  name: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      len: [5, 100]
+    }
+  },
+  description: {
+    type: TEXT,
+    allowNull: false,
+    validate: {
+      len: [140, 1400]
+    }
+  },
+  price: {
+    type: FLOAT,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  inventory: {
+    type: INTEGER,
+    allowNull: false,
+    validate: {
+    min: 0
+    }
+  },
+  photo: {
+    type: STRING,
+    defaultValue: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Hestem%C3%B8j.jpg/296px-Hestem%C3%B8j.jpg',
+    validate: {
+      isUrl: true, // watch out for problems !!
+    }
+  },
+  category: {
+    type: ENUM('animal', 'plant'),
+    allowNull: false
+  },
+  // advanced fields: date item added to the invetory (requires a join table)
+},{
+  // hooks
+  // getterMethods
+  // setterMethods
+  // classMethods
+  // instanceMethods
+})
+
+// module.exports.associations = (Thing, {User, Favorite}) => {
+//   Thing.belongsToMany(User, {as: 'lovers', through: Favorite})
+// }
