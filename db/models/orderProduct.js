4eac10e9e3922e12c3a68df290e3dfdc27903c7b
Jacob Kayen <jacob.kayen@gmail.com>
2017-04-18 14:35:52 -0400

    feat: create orderProduct model

--- db/models/orderProduct.js
+++ db/models/orderProduct.js
@@ -2,8 +2,22 @@
 
 // bcrypt docs: https://www.npmjs.com/package/bcrypt
 const bcrypt = require('bcryptjs')
-    , {STRING, VIRTUAL, BOOLEAN, INTEGER, DATE} = require('sequelize')
+    , {STRING, VIRTUAL, BOOLEAN, INTEGER, DATE, FLOAT} = require('sequelize')
 
 module.exports = db => db.define('orderProduct', {
-
+  quantity: {
+    type: INTEGER
+    // can we set default from the front end?
+    // defaultValue: 1
+  },
+  unitPrice: {
+    type: FLOAT(2)
+    // we can add this from the front end. Otherwse:
+    // set: this.setDataValue('unitPrice', this.product.price)
+  }
 })
+
+module.exports.associations = (OrderProduct, {Order, Product}) => {
+  OrderProduct.belongsTo(Order)
+  OrderProduct.belongsTo(Product)
+}
