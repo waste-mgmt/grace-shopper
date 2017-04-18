34e6810ee94b5711f0e9464704bb992203e16c51
Jacob Kayen <jacob.kayen@gmail.com>
2017-04-18 14:28:33 -0400

    create orderProduct file

---
+++ db/models/orderProduct.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// bcrypt docs: https://www.npmjs.com/package/bcrypt
+const bcrypt = require('bcryptjs')
+    , {STRING, VIRTUAL, BOOLEAN, INTEGER, DATE} = require('sequelize')
+
+module.exports = db => db.define('orderProduct', {
+
+})
