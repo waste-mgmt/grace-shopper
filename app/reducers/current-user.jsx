a71b9f26ed4ae264a96f150cac5d9cdd9f4f8d25
Jacob Kayen <jacob.kayen@gmail.com>
2017-04-24 16:34:08 -0400

    create user state

---
+++ app/reducers/current-user.jsx
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const initialCurrentUserState = {}
+
+export default function (state = initialCurrentUserState, action) {
+  switch (action.type) {
+    case SELECT_CURRENT_USER:
+      return action.currentUser
+    default:
+      return state
+  }
+}
+
+const SELECT_CURRENT_USER = 'SELECT_CURRENT_USER'
+
+const receivingCurrentUser = currentUser => ({
+  type: SELECT_CURRENT_USER,
+  currentUser
+})
+
+export const setCurrentUser = userId => {
+  return dispatch => {
+    return axios.get(`/api/users/${userId}`)
+      .then(res => {
+        dispatch(receivingCurrentUser(res.data))
+      })
+  }
+}
+
+export const updateCurrentUser = (updatedUser) => {
+  return dispatch => {
+    return axios.put(`/api/users/${updatedUser.id}`, updatedUser)
+      .then(res => {
+        dispatch(receivingCurrentUser(res.data.id));
+      });
+  }
+}
--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -6,7 +6,10 @@ const rootReducer = combineReducers({
   allOrders: require('./all-orders-admin').default,
   allUserOrders: require('./all-orders-user').default,
   selectedOrder: require('./single-order').default,
-  cart: require('./cart').default
+  cart: require('./cart').default,
+  currentUser: require('./current-user').default,
+  allUsers: require('./all-users').default,
+  selectedUser: require('./selected-User').default
 })
 
 export default rootReducer
