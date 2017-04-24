15c643e9be53840bbd27bdc454781dbcc01fac1d
ekotwick <edmond.kotwick@gmail.com>
2017-04-24 14:54:15 -0400

    closes #118, variable names, comments

--- app/reducers/single-order.js
+++ app/reducers/single-order.js
@@ -4,44 +4,72 @@ import axios from 'axios';
 
 const initialSelectedOrderState = {}
 
-// NOTES ON INITIAL STATE:
-// (1) it may be useful to keep recently created and updated orders on the state, so that a user can update orders in the same session that the order was made and avoid having to make axios requests just to achieve the same result
-// (2) it may also be useful to distinguish between `allOrders` and `allUserOrders`. *Admins* can access *all* users, but regular users can only access their order history. Perhaps we do not want to serve up all user order data to ordinary users. Besides, we have back-end routes for serving up all orders and orders for specific users;
 
 // #### REDUCER #### //
 
 export default function (state = initialSelectedOrderState, action) {
+
   switch (action.type) {
-    case SELECT_ORDER:
-      return action.selectedOrder;
+
+    case SET_ORDER:
+      return action.order;
       break;
+
     default:
       return state;
   }
+  
 };
 
+
 // #### CONSTANTS #### //
+
 // admin and auth'd user actions
-const SELECT_ORDER = 'SELECT_ORDER';
+const SET_ORDER = 'SET_ORDER';
+
 
 // #### DISPATCHERS #### //
-// selects one specific order from the state
-export const selectOrder = selectedOrder => ({
-  type: SELECT_ORDER, selectedOrder
+
+// use this dispatcher when pulling orders from the state and setting the above state with it.
+export const retrieveOrder = order => ({
+  type: SET_ORDER, order
 });
 
+
 // #### THUNKS #### //
-// Admin & auth'd user action: selects one order from state
-export const selectOneOrder = orderId => {
+
+// Admin & auth'd user action: selects one order from the database
+export const getOrder = orderId => {
   return dispatch => {
     return axios.get(`/api/orders/${orderId}`)
       .then(res => {
-        dispatch(selectOrder(res.data))
+        dispatch(retrieveOrder(res.data))
       })
-  }
-  // loop through `allOrders` array and pick the object that matches what we pass in as a function
-  // take the matching item and dispatch it to the `selectUserOrder` action creator:
-  // pseudocode: `return stateElement.filter(order => {order.identifier === orderIdentifer})`
-  // the `stateElement` is one of two arrays on the state (**if** we opt to distinguish between auth states and admin states)
-}
+  };
+};
 
+// Admin, auth'd user, and guest action: update one order
+export const putOrder = (orderId, orderDetails) => {
+  return dispatch => {
+    return axios.put(`/api/orders/${orderId}`, orderDetails) // updates the order in the database
+      .then(res => {
+        return axios.get(`/api/orders/${res.data.id}`) // queries the database, returning updated order
+          .then(res => {
+            dispatch(retrieveOrder(res.data)); // dispatches to update the state with updated order
+          });
+      });
+  };
+};
+
+// Admin, auth'd user, and guest action: create new order
+export const postOrder = orderDetails => {
+  return dispatch => {
+    return axios.post(`/api/orders`, orderDetails) // creates new row in database
+      .then(res => {
+        return axios.get(`/api/orders/${res.data.id}`) // queries the database, returning created order
+          .then(res => {
+            dispatch(retrieveOrder(res.data)); // dispatches to update the state with created order
+          })
+      });
+  }
+};
